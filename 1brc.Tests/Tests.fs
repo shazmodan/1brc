6bdb084f196bc858150a745123da26f41307842d
William Bergqvist <shazmodan@gmail.com>
2025-11-29 17:35:15 +0100

    init

---
+++ 1brc.Tests/Tests.fs
@@ -0,0 +1,30 @@
+﻿module Tests
+
+open System
+open System.IO
+open Xunit
+
+[<Fact>]
+let ``Program produces the correct output - 10 lines`` () =
+    let currentDir = __SOURCE_DIRECTORY__
+    let testFilePath = Path.Combine(currentDir, "input_10.txt")
+
+    // Setup and read program output.
+    use sw = new StringWriter()
+    Console.SetOut(sw)
+    Program.main [| testFilePath |] |> ignore
+
+    let actual = sw.ToString().TrimEnd()
+
+    let expected =
+        """Bridgetown;26.9;26.9;26.9
+Bulawayo;8.9;8.9;8.9
+Conakry;31.2;31.2;31.2
+Cracow;12.6;12.6;12.6
+Hamburg;12.0;12.0;12.0
+Istanbul;6.2;14.6;23.0
+Palembang;38.8;38.8;38.8
+Roseau;34.4;34.4;34.4
+St. John's;15.2;15.2;15.2"""
+    
+    Assert.Contains(expected, actual)
---
+++ 1brc/Program.fs
@@ -0,0 +1,66 @@
+﻿// The task is to write a Java program which reads the file, 
+// calculates the min, mean, and max temperature value per weather station, 
+// and emits the results on stdout like this (i.e. sorted alphabetically by station name, 
+// and the result values per station in the format <min>/<mean>/<max>, rounded to one fractional digit):
+
+open System
+
+type Station = string
+type Min = float
+type Count = float
+type Mean = float
+type Max = float
+type Temperature = float
+
+let printResult(map: Map<string, Min * Mean * Max * Count>) =
+    map
+    |> Map.iter (fun station (min, mean, max, _) -> 
+        printfn $"%s{station};%.1f{min};%.1f{Math.Round(mean, 1)};%.1f{max}")
+
+
+//TODO: Check if struct(min, mean, max) instead of normal tuples (allocated on stack instead) is faster.
+// let a = (1, "hej")          // System.Tuple<int,string>, heap
+// let b = struct (1, "hej")   // System.ValueTuple<int,string>, stack (usually)
+let parseLine (line: string) : Station * Temperature  =
+    let semiColonIndex = line.IndexOf ';'
+    line.Substring(0, semiColonIndex), line.Substring(semiColonIndex + 1) |> float
+
+let addLineToDict (resultMap: Map<string, Min * Mean * Max * Count>) (line: string) =
+    let station, newTemperature = parseLine line
+
+    let existingValuesOpt = resultMap.TryFind station
+
+    match existingValuesOpt with
+    | None -> 
+        resultMap 
+        |> Map.add station (newTemperature, newTemperature, newTemperature, 1.0)
+    | Some (existingMin, existingMean, existingMax, existingCount) ->
+        let newCount = existingCount + 1.0
+        let newMin = min existingMin newTemperature
+        let newMax = max existingMax newTemperature
+        let newMean = (existingMean + newTemperature) / newCount
+
+        resultMap |> Map.add station (newMin, newMean, newMax, newCount)
+
+//TODO: Prob not readalllines, one a time is best
+let parse (path: string) : Map<string, Min * Mean * Max * Count> =
+    let initMap : Map<string, Min * Mean * Max * Count> = Map.empty
+    
+    let lines: string array = System.IO.File.ReadAllLines path
+
+    (initMap, lines)
+    ||> Array.fold (fun accMap line -> addLineToDict accMap line)
+
+
+[<EntryPoint>]
+let main args =
+    if args.Length = 0 then
+        printfn "Missing file path"
+        1
+    else
+        let path: string = args[0]
+        let resultMap = parse path
+
+        printResult resultMap
+
+        0
